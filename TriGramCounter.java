48d4974abaf5e08f87da613279883e54374dc51d
DannyBoy173 <danielbaulk@gmail.com>
2021-11-10 17:22:31 +0000

    Further renaming, added comments

--- TriGramCounter.java
+++ TriGramCounter.java
@@ -12,42 +12,50 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TriGramCounter {
-    public static class WCMapper extends Mapper<Object, Text, Text, IntWritable>{
-        private final static IntWritable one = new IntWritable(1);
+    public static class TGCMapper extends Mapper<Object, Text, Text, IntWritable>{
+        private final static IntWritable one = new IntWritable(1); // defines the number 1 to be counted
         private Text word = new Text();
         
+        // mapper function
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString(), " \t\n\r\f,.:;?![]'"); //removes punctuation from tokeneised strings
+            StringTokenizer itr = new StringTokenizer(value.toString(), " \t\n\r\f,.:;?![]'"); // separates line into list of words based on criteria
             
+            // iterate through each word in the tokenized list
             while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
+                word.set(itr.nextToken()); // set the word
+                context.write(word, one); // assign value 'one' to word
             }
         }
     }
     
-    public static class WCReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    public static class TGCReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
         private IntWritable result = new IntWritable();
         
+        // reducer function
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
+
+            // for each key, aggregate the values (just adds all the ones together)
             for (IntWritable val : values) {
                 sum += val.get();
             }
-            result.set(sum);
-            context.write(key, result);
+
+            result.set(sum); // set result to be sum
+            context.write(key, result); // output: key, result
         }
     }
     public static void main(String[] args) throws Exception {
+        // preparing environment for hadoop job
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(TriGramCounter.class);
-        job.setMapperClass(WCMapper.class);
-        job.setReducerClass(WCReducer.class);
+        Job job = Job.getInstance(conf, "tri gram count");
+
+        job.setJarByClass(TriGramCounter.class); // implementation of job
+        job.setMapperClass(TGCMapper.class); // mapper class
+        job.setReducerClass(TGCReducer.class); // reducer class
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        FileInputFormat.addInputPath(job, new Path(args[0])); // define where the input is specified - from CL
+        FileOutputFormat.setOutputPath(job, new Path(args[1])); // define where the output should go - from CL
+        System.exit(job.waitForCompletion(true) ? 0 : 1); // finish up the job
     }
 }
